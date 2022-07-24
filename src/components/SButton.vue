78cd1439495d08d7735dbe1c88fad60ebdc889ff
karleea <karlee.aguirre@icloud.com>
2022-07-24 15:23:27 -0400

    Refactor SEventCard to use table, add buttons that interact with API

---
+++ src/components/SButton.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'SButton'
+}
+</script>
+
+<template>
+    <button>
+        <slot name="value"/>
+    </button>
+</template>
+
+<!-- TODO: Style button -->
+<style lang="scss" scoped>
+</style>
--- src/components/SEventCard.vue
+++ src/components/SEventCard.vue
@@ -7,54 +7,34 @@ export default {
 
 <template>
 <div class="card">
-    <slot name="eventtitle"></slot>       
-        <div class="content">
-            
-            <div class="column-one">
-                <br>
-                <slot name="teams"></slot>
-            </div> 
-            <div class="column-two">
-                ML
-                <br>
-                 <slot name="ml"></slot>
-            </div>
-            <div class="column-three">
-                Spread
-                <br>
-              <slot name="spread"></slot>
-            </div> 
-        </div>
+    <table>
+        <tr><slot name="eventTitle"/></tr>
+        <tr>
+            <th> </th>
+            <th>ML </th>
+            <th>Spread</th>
+        </tr>
+        <tr>
+            <td><slot name="teamOneName"/></td>
+            <td><slot name="teamOneML"/></td>
+            <td><slot name="teamOneSpread" /></td>
+        </tr>
+        <tr>
+            <td><slot name="teamTwoName"/></td>
+            <td><slot name="teamTwoML"/></td>
+            <td><slot name="teamTwoSpread" /></td>
+        </tr>
+    </table>
 </div> 
 
 </template>
 <style lang="scss" scoped>
-
-.content {
-  display: grid;
-  grid-column-gap: 1em;
-  grid-row-gap: 3em;
-
-}
 
 .card {
 border: 2px solid #000000;
 border-radius: 4px;
 padding: .5rem;
  
-}
-
-
-.column-one {
-    grid-column:1;
-}
-
-.column-two {
-    grid-column: 2;
-}
-
-.column-three {
-    grid-column: 3;
 }
 
 </style>
